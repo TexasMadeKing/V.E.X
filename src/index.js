074c84e108723433eb3a7610cef0f0420163a06d
Josh Bennetsen <65803112+TexasMadeKing@users.noreply.github.com>
2024-01-31 13:55:11 -0600

    Planning Updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import AboutPage from './components/pages/AboutPage';
+import ContactPage from './components/pages/ContactPage';
+import Login from './components/pages/Login';
+import SignUp from './components/pages/SignUp';
+import Profile from './components/pages/Profile';
+import Resume from './components/pages/Resume';
+import DocumentsUpload from './components/pages/DocumentsUpload';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+    return (
+        <BrowserRouter>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/signup">SignUp</Link>
+                        </li>
+                        <li>
+                            <Link to="/profile">Profile</Link>
+                        </li>
+                        <li>
+                            <Link to="/resume">Resume</Link>
+                        </li>
+                        <li>
+                            <Link to="/DocumentsUpload">DocumentsUpload</Link> 
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/contact">Contact</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/about" component={AboutPage} />
+                    <Route path="/contact" component={ContactPage} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/signup" component={SignUp} />
+                    <Route path="/profile" component={Profile} />
+                    <Route path="/resume" component={Resume} />
+                    <Route path="/DocumentsUpload" component={DocumentsUpload} />
+                    <Route component={HomePage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
